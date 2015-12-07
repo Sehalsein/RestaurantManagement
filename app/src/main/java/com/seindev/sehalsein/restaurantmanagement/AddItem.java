176e3c5b110b13b14ccc197eb17fbf8392b95410
Sehalsein <seindeveloper@gmail.com>
2015-12-07 21:25:44 +0530

    Add Item
    Version 0.0.0.3
    
    -Removed Recycler View
    -Direct Access to Edit Text
    -Minor Bug Fixes And UI Fixes

--- app/src/main/java/com/seindev/sehalsein/restaurantmanagement/AddItem.java
+++ app/src/main/java/com/seindev/sehalsein/restaurantmanagement/AddItem.java
@@ -8,10 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,8 +22,11 @@ import java.util.List;
 
 public class AddItem extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    TextView textView;
+    private EditText vDishName, vPrice, vQuantity;
+    private AutoCompleteTextView vCategory;
+    private MultiAutoCompleteTextView vTags;
+    private String[] categories;
+    private String[] tags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +37,37 @@ public class AddItem extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //RECYCLER VIEW
-        recyclerView = (RecyclerView) findViewById(R.id.add_item_recycler);
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager linearLayout = new LinearLayoutManager(this);
-        linearLayout.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(linearLayout);
-        AddItemAdapter addItemAdapter = new AddItemAdapter(this, createList(7));
-        recyclerView.setAdapter(addItemAdapter);
+        //INITIALIZING
+        vDishName = (EditText) findViewById(R.id.editDishName);
+        vPrice = (EditText) findViewById(R.id.editPrice);
+        vQuantity = (EditText) findViewById(R.id.editQuantity);
+        vCategory = (AutoCompleteTextView) findViewById(R.id.autocompletetextCategory);
+        vTags = (MultiAutoCompleteTextView) findViewById(R.id.multiautocompletetextTags);
+
+        //ARRAY
+        categories = getResources().getStringArray(R.array.categories);
+        tags = getResources().getStringArray(R.array.tags);
+
+        //INPUT TYPES
+        //DISHNAME
+        vDishName.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+
+        //PRICE
+        vPrice.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        //QUANTITY
+        vQuantity.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        //CATEGORY
+        ArrayAdapter<String> mcategories = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categories);
+        vCategory.setAdapter(mcategories);
+        vCategory.setThreshold(1);
+
+        //TAGS
+        ArrayAdapter<String> mtags = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tags);
+        vTags.setAdapter(mtags);
+        vTags.setThreshold(1);
+        vTags.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 
 
     }
@@ -47,48 +75,44 @@ public class AddItem extends AppCompatActivity {
 
     public void Additem(View view) {
 
-        Toast.makeText(this, "WAZAAAAAAP", Toast.LENGTH_LONG).show();
-
-    }
-
-    //TEMP TEST LIST
-    private List<AddItemInfo> createList(int size) {
-        List<AddItemInfo> result = null;
+        String mDishName, mCategory, mTags;
+        String mPrice, mQuantity;
 
         try {
+            mCategory = String.valueOf(vCategory.getText());
+            mDishName = String.valueOf(vDishName.getText());
+            mPrice = vPrice.getText().toString();
+            mQuantity = vQuantity.getText().toString();
+            mTags = String.valueOf(vTags.getText());
 
-            String vItemHint[] = {
-                    "Dish Name",
-                    "Price (\u20B9)",       //RUPEE SYMBOL
-                    "Quantity (no.of person)",
-                    "Category",
-                    "Tags",
-            };
-
-            String vItemName[] = {
-                    "Dish Name",
-                    "Price",
-                    "Quantity",
-                    "Category",
-                    "Tags",
-            };
-
-
-            result = new ArrayList<AddItemInfo>();
-            for (int i = 0; i < size; i++) {
-
-                AddItemInfo addItemInfo = new AddItemInfo();
-                addItemInfo.vItemHint = vItemHint[i];
-                addItemInfo.vItemName = vItemName[i];
-
-
-                result.add(addItemInfo);
+            if (vCategory.getText().toString().trim().length() <= 0) {
+                mCategory = "";
             }
-        } catch (Exception e) {
+            if (vDishName.getText().toString().trim().length() <= 0) {
+                mDishName = "";
+            }
+            if (vTags.getText().toString().trim().length() <= 0) {
+                mTags = "";
+            }
+            if (vPrice.getText().toString().trim().length() <= 0) {
+                mPrice = "";
+            }
+            if (vQuantity.getText().toString().trim().length() <= 0) {
+                mQuantity = "";
+            }
 
+
+            if (mCategory == "" || mDishName == "" || mTags == "" || mPrice == "" || mQuantity == "")
+                Toast.makeText(this, "FIELD EMPTY ", Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(this, "ITEM INSERTED", Toast.LENGTH_LONG).show();
+
+
+        } catch (NullPointerException n) {
+            Toast.makeText(this, "" + n.toString(), Toast.LENGTH_LONG).show();
         }
 
-        return result;
+
     }
 
 }
