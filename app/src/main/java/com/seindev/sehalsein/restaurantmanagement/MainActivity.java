43bc5e704a4519f16ac6810e1e6508974a4408de
Sehalsein <sehal11@hotmail.com>
2015-12-26 09:20:36 +0400

    Checking
    Checking

--- app/src/main/java/com/seindev/sehalsein/restaurantmanagement/MainActivity.java
+++ app/src/main/java/com/seindev/sehalsein/restaurantmanagement/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //TESTING CODE
+
 
     }
 
